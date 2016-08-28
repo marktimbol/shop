033afef5814ab898f330247068c61ea1f1d3b38c
Mark Timbol <Mark@Mark.local>
2016-08-28 17:31:22 +0400

    Behold. The homepage design is coming.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
     <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
     @yield('header')
 
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,121 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="Home">
+    	<div class="Home__bg">
+	        <div class="container">
+	            <div class="row">
+	            	<div class="col-md-12">
+	                	<figure class="Figure__reverse Figure--with-padding">
+	                		<div class="Figure__right">
+	                			<div class="Figure__content">
+				                    <img src="/images/slide-watch.png" 
+				                    	alt="Slide watch" 
+				                    	title="Watch" 
+				                    	class="img-responsive" />
+			                    </div>
+	                		</div>
+
+	                		<figcaption class="Figure__left">
+		                        <h3>Welcome to Longacee Brand</h3>
+		                        <h2>Premium Watches For Men</h2>
+		                        <p>
+		                            Neque porro quisquam est qui dolorem ipsum quia tulaxianmi dolor sit amet, consectetur adipisci velit.
+		                        </p>
+		    
+		                        <a href="/shop" class="btn btn-default action-button">Shop now</a>
+	                		</figcaption>
+	                	</figure>
+	                </div>
+	            </div>
+
+	            <div class="row">
+	                <div class="col-md-12">
+	                	<figure>
+	                		<div class="Figure__left">
+		                		<img src="/images/Home-model-1.jpg" 
+		                			alt="Model" 
+		                			title="Model" 
+		                			class="img-responsive" />
+	                		</div>
+
+	                		<figcaption class="Figure__right Figure--with-dark-bg">
+	                			<div class="Figure__content">
+				                    <h3>Here is our stories</h3>
+				                    <h2>Find your perfect logan watch</h2>
+				                    <p>
+				                        Neque porro quisquam est qui dolorem ipsum quia tulaxianmi dolor sit amet, consectetur, adipisci velit. Neque porro quisqu teaam est qui dolorem ipsum quia tulaxianmi dolor sit amet, consectetur, adipisci velit.
+				                    </p>
+
+				                    <p>
+				                        <a href="/shop" class="btn btn-default action-button">See more</a>
+				                    </p>
+			                    </div>
+	                		</figcaption>
+	                	</figure>
+	                </div>
+	            </div>
+
+	            <div class="row">
+	                <div class="col-md-12">
+	                	<figure class="Figure__reverse">
+	                		<div class="Figure__left">
+	                    		<img src="/images/Home-hublot.jpg" alt="Hublot" title="Hublot" class="img-responsive" />
+	                		</div>
+
+	                		<figcaption class="Figure__right Figure--with-dark-bg">
+	                			<div class="Figure__content">
+				                    <h3>Discover the awesomes of Logancee</h3>
+				                    <h2>Exquisite Beauty of Hublot</h2>
+
+				                    <p>
+				                        Neque porro quisquam est qui dolorem ipsum quia tulaxianmi dolor sit amet, consectetur, adipisci velit.
+				                    </p>
+
+				                    <p class="signature">
+				                    	<img src="/images/signature.png" alt="Signature" title="Signature" class="img-responsive" />
+				                    </p>
+			                    </div>
+	                		</figcaption>
+	                	</figure>
+	                </div>
+	            </div>
+	        </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                	<figure>
+                		<div class="Figure__left">
+                    		<img src="/images/Home-model-2.jpg" alt="Model" title="Model" class="img-responsive" />
+                		</div>
+                		<figcaption class="Figure__right Figure--with-dark-bg">
+                			<div class="Figure__content">
+    			                <h3>Here is our stories</h3>
+    			                <h2>Find your perfect logan watch</h2>
+    			                <p>
+    			                    Neque porro quisquam est qui dolorem ipsum quia tulaxianmi dolor sit amet, consectetur, adipisci velit. Neque porro quisqu teaam est qui dolorem ipsum quia tulaxianmi dolor sit amet, consectetur, adipisci velit.
+    			                </p>
+    			                <p>
+    			                    <a href="/shop" class="btn btn-default action-button">See more</a>
+    			                </p>            
+    	                    </div>
+                		</figcaption>
+                	</figure>
+                </div>
+            </div>
+        </div>
+    </div>  
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="text-center">Popular our product</h2>
+			</div>
+		</div>
+	</div>
+
 	<ul>
 		@foreach($items as $item)
 			<li>

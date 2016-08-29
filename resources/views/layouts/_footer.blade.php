13cf0aad9d818716b39773911fadbacc48100337
Mark Timbol <Mark@Mark.local>
2016-08-29 15:21:56 +0400

    Working on the front-end

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,41 +1,48 @@
 <footer>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-4">
-				<h4>Contact Us</h4>
-				<p>
-					Room 708 Clock Plaza, Al Rigga, Dubai, UAE<br />
-					Email: mark.timbol@hotmail.com<br />
-					Mobile: +971 56 375 9865
-				</p>
-			</div>
+			<div class="col-md-12">
+				<div class="Footer__contents">
+					<div class="Footer__content">
+						<h4>Contact Us</h4>
+						<p>
+							Room 708 Clock Plaza, Al Rigga, Dubai, UAE<br />
+							Email: mark.timbol@hotmail.com<br />
+							Mobile: +971 56 375 9865
+						</p>
+					</div>
 
-			<div class="col-md-4">
-				<div class="footer-logo">
-					<h4>
-						<img src="/images/footer-logo.png" alt="Logo" title="Logo" class="img-responsive" />
-					</h4>
+					<div class="Footer__content">
+						<div class="footer-logo">
+							<h4>
+								<img src="/images/footer-logo.png" alt="Logo" title="Logo" class="img-responsive" />
+							</h4>
+						</div>
+						<h6>Payment options</h6>
+						<img src="/images/payment-options.png" 
+							alt="Payment Options" 
+							title="Payment Options" 
+							class="img-responsive" />
+					</div>
+
+					<div class="Footer__content">
+						<h4 class="text-right">Latest Blog</h4>
+						<ul>
+							<li>
+								<a href="#">Trending for 2015!</a>
+							</li>
+							<li>
+								<a href="#">New shoes for trekking boys</a>
+							</li>
+							<li>
+								<a href="#">Street style with logancee fashion</a>
+							</li>					
+							<li>
+								<a href="http://logancee15.typostores.xyz/" target="_blank">The Inspiration</a>
+							</li>
+						</ul>
+					</div>
 				</div>
-				<h6>Payment options</h6>
-				<img src="/images/payment-options.png" alt="Payment Options" title="Payment Options" class="img-responsive" />
-			</div>
-
-			<div class="col-md-4">
-				<h4 class="text-right">Latest Blog</h4>
-				<ul>
-					<li>
-						<a href="#">Trending for 2015!</a>
-					</li>
-					<li>
-						<a href="#">New shoes for trekking boys</a>
-					</li>
-					<li>
-						<a href="#">Street style with logancee fashion</a>
-					</li>					
-					<li>
-						<a href="http://logancee15.typostores.xyz/" target="_blank">The Inspiration</a>
-					</li>
-				</ul>
 			</div>
 		</div>
 	</div>
